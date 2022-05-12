42a8287e1f1352c18e396c1b9c5f36b9f3efd2c1
Wojciech Wroński <hierodrimm@gmail.com>
2022-05-12 13:24:44 +0200

    Add spawn save point

--- Assets/Scripts/SavePoint.cs
+++ Assets/Scripts/SavePoint.cs
@@ -3,8 +3,17 @@ using UnityEngine;
 public class SavePoint : MonoBehaviour
 {
     [SerializeField] private Transform spawnPosition;
+    [SerializeField] private bool isStartSpawn = false;
 
     public Transform SpawnPosition { get => spawnPosition; set => spawnPosition = value; }
+
+    private void Awake()
+    {
+        if (isStartSpawn)
+        {
+            FindObjectOfType<PlayerController>().LastCheckpoint = this;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
